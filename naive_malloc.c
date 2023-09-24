5827c369771b555a47cb72e516020461e4c6da76
oladele90 <oladelemiro07@gmail.com>
2023-09-24 17:12:51 +0000

    change back to old implementation

--- naive_malloc.c
+++ naive_malloc.c
@@ -12,12 +12,12 @@ size_t chunker(size_t size)
 
 void *naive_malloc(size_t size)
 {
-    void *start_brk = sbrk(0), *end_brk = sbrk(_SC_PAGESIZE), *new_brk;
+    void *start_brk = sbrk(0), *new_brk;
 
     size = chunker(size);
     sbrk(size + sizeof(size_t));
     new_brk = sbrk(0);
     *((size_t *)start_brk) = size + sizeof(size_t);
     start_brk = (char *)new_brk - size;
-    return(end_brk);
+    return(start_brk);
 }
