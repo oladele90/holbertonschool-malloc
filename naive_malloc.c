27a545dd8f5734111b38dc2a7fbb433f4d91c47b
oladele90 <oladelemiro07@gmail.com>
2023-09-24 19:00:09 +0000

    adds pagesize

--- naive_malloc.c
+++ naive_malloc.c
@@ -12,18 +12,20 @@ size_t chunker(size_t size)
 
 void *naive_malloc(size_t size)
 {
-    void *start_brk = sbrk(0), *new_brk;
-    static size_t pagesize = 0;
-    if(pagesize < size)
-    {
-        sbrk(_SC_PAGESIZE);
-        pagesize += sysconf(_SC_PAGESIZE);
-    }
+    static void *start_brk;
+    void *new_brk;
+    static size_t page;
+
+    if (!start_brk)
+        start_brk = sbrk(0);
     size = chunker(size);
-    sbrk(size + sizeof(size_t));
-    new_brk = sbrk(0);
+    if (page < size)
+    {
+        sbrk(sysconf(_SC_PAGESIZE));
+        page += (sysconf(_SC_PAGESIZE));
+    }
     *((size_t *)start_brk) = size + sizeof(size_t);
+    new_brk = sbrk(*(size_t *)start_brk);
     start_brk = (char *)new_brk - size;
-    pagesize -= (size + sizeof(size_t));
     return(start_brk);
 }
