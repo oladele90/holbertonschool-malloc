d4072a68977b58358c6f6c967417d0b91cf411f9
oladele90 <oladelemiro07@gmail.com>
2023-09-24 20:13:32 +0000

    re implement

--- naive_malloc.c
+++ naive_malloc.c
@@ -13,19 +13,21 @@ size_t chunker(size_t size)
 void *naive_malloc(size_t size)
 {
     static void *start_brk;
-    void *new_brk;
+    void * new_brk;
+    size_t pagesize = sysconf(_SC_PAGESIZE);
     static size_t page;
 
     if (!start_brk)
         start_brk = sbrk(0);
-    size = chunker(size);
-    if (page < size)
+    while (page < size)
     {
-        sbrk(sysconf(_SC_PAGESIZE));
-        page += (sysconf(_SC_PAGESIZE));
+        page += pagesize;
+        sbrk(pagesize);
     }
+    size = chunker(size);
     *((size_t *)start_brk) = size + sizeof(size_t);
-    new_brk = sbrk(*(size_t *)start_brk);
-    start_brk = (char *)new_brk - size;
-    return(start_brk);
+    new_brk = (char *)start_brk + sizeof(size_t);
+    start_brk = (char *) start_brk + size + sizeof(size_t);
+    page -= (size + sizeof(size_t));
+    return(new_brk);
 }
